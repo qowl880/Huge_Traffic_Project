6596f7ce574bf455f39da9bede909de7ea5e9f6c
JIWON <82360230+Bae-Ji-Won@users.noreply.github.com>
2025-04-02 20:54:19 +0900

    Revert "Coupon service feature/coupon create"

--- promotion/coupon-service/src/main/java/com/traffic/couponservice/exception/CouponPolicyNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.traffic.couponservice.exception;
-
-public class CouponPolicyNotFoundException extends RuntimeException{
-    public CouponPolicyNotFoundException(String message){
-        super(message);
-    }
-}
