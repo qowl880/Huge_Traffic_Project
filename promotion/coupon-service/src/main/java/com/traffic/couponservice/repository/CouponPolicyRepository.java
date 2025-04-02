dd5d7b22a59990b503651982309be722ab04dd89
JIWON <qowhxk@naver.com>
2025-04-02 20:41:52 +0900

    주석 추가

--- promotion/coupon-service/src/main/java/com/traffic/couponservice/repository/CouponPolicyRepository.java
+++ promotion/coupon-service/src/main/java/com/traffic/couponservice/repository/CouponPolicyRepository.java
@@ -10,7 +10,8 @@ import java.util.Optional;
 
 public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)                           // 비관적 락 - 잠금이 설정된 동안에는 현재 업무가 끝나기 전까지 다른 api가 들어와도 대기상대가 됨
+    // 비관적 락 - 잠금이 설정된 동안에는 현재 업무가 끝나기 전까지 다른 api가 들어와도 대기상대가 됨
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT cp FROM CouponPolicy cp WHERE cp.id = :id")
     Optional<CouponPolicy> findByIdWithLock(Long id);
 }
