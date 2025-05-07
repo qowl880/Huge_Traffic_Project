49c28cbe75f8124861b41b0a8df078683dce95d6
Bae Ji Won <qowhxk@naver.com>
2025-05-07 13:33:16 +0900

    Point-Service-Batch Init Commit

---
+++ promotion/point-service-batch/src/main/java/com/traffic/pointservicebatch/PointServiceBatchApplication.java
@@ -0,0 +1,13 @@
+package com.traffic.pointservicebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointServiceBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointServiceBatchApplication.class, args);
+	}
+
+}
---
+++ promotion/point-service-batch/src/test/java/com/traffic/pointservicebatch/PointServiceBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traffic.pointservicebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointServiceBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
