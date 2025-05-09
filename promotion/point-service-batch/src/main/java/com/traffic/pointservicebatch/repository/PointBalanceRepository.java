2c575d66d9fc20d3ab938ad91c70e0d794742ade
JIWON <qowhxk@naver.com>
2025-05-09 13:23:36 +0900

    Repository ADD

--- promotion/point-service-batch/src/main/java/com/traffic/pointservicebatch/repository/PointBalanceRepository.java
+++ promotion/point-service-batch/src/main/java/com/traffic/pointservicebatch/repository/PointBalanceRepository.java
@@ -3,5 +3,8 @@ package com.traffic.pointservicebatch.repository;
 import com.traffic.pointservicebatch.domain.PointBalance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PointBalanceRepository extends JpaRepository<PointBalance, Long> {
+    Optional<PointBalance> findByUserId(Long userId);
 }
